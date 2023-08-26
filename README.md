a9c487f369131299b34fecff5c8dc1e8d550f83a
Satya_Nayak <satyanayak289@gmail.com>
2023-08-26 13:17:58 +0530

    Satya

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # git-sample-project-1
+
 Sample Project for Git events
 
 Sample Project 1 is a triangle identifier.
 triangle.py contains the program.
 
 Fix the errors, and submit a Pull Request.
+
+The errors are fixed,Another class of traingle is added and also the input type is made float
--- triangle.py
+++ triangle.py
@@ -3,17 +3,22 @@
 #
 
 # triangle.py accepts the lenghts of 3 sides and outputs the kind of triangle
+import sys
 
-a=input("Enter side 1 : ")
-b=input("Enter side 2 : ")
-c=input("Enter side 3 : ")
+a = float(input("Enter side 1 : "))
+b = float(input("Enter side 2 : "))
+c = float(input("Enter side 3 : "))
 
-if ( a*a + b*b == c*c ) or ( b*b + c*c == a*a ) :
+if ( a*a + b*b == c*c ) or ( b*b + c*c == a*a ) or ( c**2 + a**2 == b**2 ):
     print("Right Angled Triangle")
-
-if a == b :
+    sys.exit()
+    
+if a == b == c :
+    print("Equilateral Triangle")
+    sys.exit()
+    
+if a == b or b==c or a == c:
     print("Isosceles Triangle")
-
-if a != b and b != c :
+    
+if a != b and b != c and a != c:
     print("Scalene Triangle")
-
