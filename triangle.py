f28498664b214a3607c310a8c0f8713cadc5bc26
Keerthi Sureka <keerthisureka03@gmail.com>
2023-08-26 22:12:39 +0530

    issue resolved

--- triangle.py
+++ triangle.py
@@ -4,16 +4,15 @@
 
 # triangle.py accepts the lenghts of 3 sides and outputs the kind of triangle
 
-a=input("Enter side 1 : ")
-b=input("Enter side 2 : ")
-c=input("Enter side 3 : ")
+a = int(input("Enter side 1: "))
+b = int(input("Enter side 2: "))
+c = int(input("Enter side 3: "))
 
-if ( a*a + b*b == c*c ) or ( b*b + c*c == a*a ) :
+if (a*a + b*b == c*c) or (b*b + c*c == a*a) or (a*a + c*c == b*b):
     print("Right Angled Triangle")
 
-if a == b :
+elif a == b or b == c or a == c:
     print("Isosceles Triangle")
 
-if a != b and b != c :
+elif a != b and b != c and a != c:
     print("Scalene Triangle")
-
